264d83de7c9cab3114a8d19ea47dadfe515383df
Aryan Sjet <sjet@asjet.dev>
2024-07-03 16:17:06 +0800

    feat(bin/radis): read conf

--- src/bin/radis/main.rs
+++ src/bin/radis/main.rs
@@ -1,7 +1,19 @@
-use radis::raft::{Log, RequestVoteArgs, RequestVoteReply};
-use radis::raft::{RaftClient, RaftServer};
+use clap::Parser;
+use radis::conf::Config;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Config file path
+    #[arg(short, long)]
+    conf: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    let cfg = Config::from_path(&args.conf)?;
+    dbg!(cfg);
+
     Ok(())
 }
