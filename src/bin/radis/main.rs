4755cf3d1a394423b494c258430bae1d50ad4cae
Aryan Sjet <sjet@asjet.dev>
2024-07-03 16:00:53 +0800

    feat: define protobuf

---
+++ src/bin/radis/main.rs
@@ -0,0 +1,7 @@
+use radis::raft::{Log, RequestVoteArgs, RequestVoteReply};
+use radis::raft::{RaftClient, RaftServer};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,7 @@
 pub mod conf;
+
+pub mod raft {
+    tonic::include_proto!("raft");
+    pub use raft_client::RaftClient;
+    pub use raft_server::RaftServer;
+}
