ebe95fd238d292704174d0a3bf3cce9cc88f6ab4
Aryan Sjet <sjet@asjet.dev>
2024-07-07 19:12:52 +0800

    refactor(raft/config): extract closure type

--- src/raft/config.rs
+++ src/raft/config.rs
@@ -10,9 +10,15 @@ pub const ELECTION_TIMEOUT_DELTA: i64 = 75;
 pub const HEARTBEAT_TIMEOUT: i64 = 150;
 pub const HEARTBEAT_TIMEOUT_DELTA: i64 = 75;
 
-pub fn with_random_backoff(base: i64, delta: i64) -> Box<dyn Fn() -> Duration + Send + Sync> {
+pub type DurationGenerator = Box<dyn Fn() -> Duration + Send + Sync>;
+
+pub fn with_random_backoff(base: i64, delta: i64) -> DurationGenerator {
     Box::new(move || {
         let mut rng = rand::thread_rng();
         Duration::from_millis(base.add(rng.gen_range(-delta..delta)) as u64)
     })
+}
+
+pub fn with_fix(interval: i64) -> DurationGenerator {
+    Box::new(move || Duration::from_millis(interval as u64))
 }
